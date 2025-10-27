d1cb44ff17222ee28c01a687568af83c7d11e8a4
shioaws01 <shioaws01@outlook.com>
2025-10-27 11:47:31 +0800

    add delete

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    // 添加自定义的 NSIS 卸载脚本
+    println!("cargo:rustc-env=TAURI_NSIS_PREINSTALL=");
+    println!("cargo:rustc-env=TAURI_NSIS_POSTINSTALL=");
+    println!("cargo:rustc-env=TAURI_NSIS_UNINSTALL=RMDir /r /REBOOTOK $INSTDIR");
+    
     tauri_build::build()
 }
